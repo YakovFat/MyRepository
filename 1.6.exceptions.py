9e7463287b32a0a8a2dd22f29e80481cfc764cc2
YakovFat <44905135+YakovFat@users.noreply.github.com>
2018-11-16 20:32:14 +0300

    3d5.0

--- 1.6.exceptions.py
+++ 1.6.exceptions.py
@@ -1,8 +1,8 @@
-print('Используйте только положительные числа')
+print('Через пробел введите операцию и два числа.\nИспользуйте только положительные числа')
 
 try:
     a = input().split()
-    b = int(a[1])
+    assert int(a[1]) > 0 and int(a[2]) >= 0
     if a[0] == '+':
         print(int(a[1]) + int(a[2]))
     elif a[0] == '-':
@@ -11,19 +11,13 @@ try:
         print(int(a[1]) * int(a[2]))
     elif a[0] == '/':
         print(int(a[1]) / int(a[2]))
-
+    else:
+        print('Данная операция не поддерживается')
 except ValueError:
-    print("Введены некорректные данные")
-
-# assert int(a[1]) > 0 or int(a[2]) > 0, 'Вы ввели отрицательное знначение'
-
-
-# if a[0] == '+':
-#     print(int(a[1])+int(a[2]))
-# elif a[0] == '-':
-#     print(int(a[1])-int(a[2]))
-# elif a[0] == '*':
-#     print(int(a[1])*int(a[2]))
-# elif a[0] == '/':
-#     print(int(a[1])/int(a[2]))
-
+    print('Введены некорректные данные')
+except AssertionError:
+    print('Вы ввели отрицательное значение')
+except IndexError:
+    print('Введены некорректные данные')
+finally:
+    print('Программа завершена')
