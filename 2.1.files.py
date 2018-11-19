315e71d7f5960d393dc315d88a975d32451e87ca
YakovFat <44905135+YakovFat@users.noreply.github.com>
2018-11-19 21:32:32 +0300

    4D6.5EXTRA

--- 2.1.files.py
+++ 2.1.files.py
@@ -1,18 +1,35 @@
 cook_book = {}
-bbb = []
+ingredients = []
 with open('recipe_book.txt') as f:
 	for line in f:
 		name = line.strip()
 		number = f.readline().strip()
 		for ing in range(int(number)):
 			ing = f.readline().strip()
-			a = []
-			a.append(ing.split(' | '))
-			aaa = {'ingridient_name': a[0][0],'quantity': int(a[0][1]), 'measure': a[0][2]}
-			bbb.append(aaa)
-		
+			ing_list = []
+			ing_list.append(ing.split(' | '))
+			ing_dict = {'ingridient_name': ing_list[0][0],'quantity': int(ing_list[0][1]), 'measure': ing_list[0][2]}
+			ingredients.append(ing_dict)
+		cook_book[name] = ingredients
+		ingredients = []
 		f.readline()
-	cook_book[name] = bbb
-print(cook_book)
-	
+	print(cook_book, '\n'*3)
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+	dish_user = {}
+	for dish in dishes:
+		if dish in cook_book:
+			for i in cook_book[dish]:
+				dish_key = i['ingridient_name']
+				dish_user[dish_key] = {'measure': i['measure'], 'quantity':int(i['quantity'])*person_count}
+	print(dish_user)
+
+
+
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+
+
 
